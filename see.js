d6bea282e34065ee4178a00afbc0e5b61781fc92
RachAppWeb <tryinganything12@gmail.com>
2023-07-09 14:42:41 +0100

    trying6

--- see.js
+++ see.js
@@ -1,5 +1,5 @@
 // Setup UI
-const baseUrl = "https://tarmeezacademy.com/api/v1"; 
+const baseUrl = "https://tarmeezacademy.com/api/v1";
 let boton = document.getElementById("boton");
 // let commentform = document.getElementById("commentform");
 
@@ -92,7 +92,7 @@ function registerBtn() {
     Accept: "multipart/form-data",
   };
   //console.log(rname, rusername, rpassword);
-  loaderFunc(true)
+  loaderFunc(true);
   fetch(`${baseUrl}/register`, {
     method: "POST",
     body: formdatA,
@@ -114,15 +114,15 @@ function registerBtn() {
       showAlert(`You logged in seccusfully :) Welcome back`, `success`);
     })
     .catch((error) => showAlert(`${error.message}:(`, `danger`))
-    .finally(()=>{
-      loaderFunc(false)
-  });
+    .finally(() => {
+      loaderFunc(false);
+    });
 }
 //end Register
 function loginBtn() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-   loaderFunc(true)
+  loaderFunc(true);
   fetch(`${baseUrl}/login`, {
     method: "POST",
     body: JSON.stringify({ username: `${username}`, password: `${password}` }),
@@ -144,9 +144,9 @@ function loginBtn() {
       showAlert(`You logged in seccusfully :) Welcome back`, `success`);
     })
     .catch((error) => showAlert(`${error.message}:(`, `danger`))
-    .finally(()=>{
-      loaderFunc(false)
-  });
+    .finally(() => {
+      loaderFunc(false);
+    });
 }
 function logOutBtn() {
   localStorage.removeItem("token");
@@ -157,146 +157,139 @@ function logOutBtn() {
 
 // ===============
 function editPost(postOBJ) {
-  let postInfo = postOBJ
+  let postInfo = postOBJ;
   //console.log(postInfo);
-  loaderFunc(true)
-  axios.get(`${baseUrl}/posts/${postOBJ}`)
-          
-  .then(function(response){
-    
-   //console.log(response.data.data) 
-   let titlo=response.data.data.title 
-   const bodyo=response.data.data.body 
-   const imago=response.data.data.image
-   let id= response.data.data.id
-   document.getElementById("postID").value=id
-   document.getElementById("create_title").innerHTML = "Edit Post";
-   document.getElementById("add").innerHTML="Edit"
-   document.getElementById("pimage").src+=imago
-   document.getElementById("pbody").value=bodyo
-   document.getElementById("ptitle").value=titlo
-  let postModal = new bootstrap.Modal(
-    document.getElementById("pexampleModale"),
-    {}
-  );
-  postModal.toggle();
-}) .catch((error)=>{
-  showAlert(`${error.response.data.message}`, `danger`)
-}).finally(()=>{
-  loaderFunc(false)
-})
+  loaderFunc(true);
+  axios
+    .get(`${baseUrl}/posts/${postOBJ}`)
+
+    .then(function (response) {
+      //console.log(response.data.data)
+      let titlo = response.data.data.title;
+      const bodyo = response.data.data.body;
+      const imago = response.data.data.image;
+      let id = response.data.data.id;
+      document.getElementById("postID").value = id;
+      document.getElementById("create_title").innerHTML = "Edit Post";
+      document.getElementById("add").innerHTML = "Edit";
+      document.getElementById("pimage").src += imago;
+      document.getElementById("pbody").value = bodyo;
+      document.getElementById("ptitle").value = titlo;
+      let postModal = new bootstrap.Modal(
+        document.getElementById("pexampleModale"),
+        {}
+      );
+      postModal.toggle();
+      loaderFunc(false);
+    })
+    .catch((error) => {
+      showAlert(`${error.response.data.message}`, `danger`);
+    })
+    .finally(() => {
+      loaderFunc(false);
+    });
 }
 function deletePost(thePostId) {
-  document.getElementById("posID").value=thePostId
+  document.getElementById("posID").value = thePostId;
   let postModal = new bootstrap.Modal(
     document.getElementById("dexampleModale"),
     {}
   );
   postModal.toggle();
-  
 }
 
-function deletePostBtn(){
-  let posID=document.getElementById("posID").value
+function deletePostBtn() {
+  let posID = document.getElementById("posID").value;
   const token = localStorage.getItem("token");
   const headers = {
-   
     authorization: `Bearer ${token}`,
   };
-  
-  axios.delete(`${baseUrl}/posts/${posID}`,{
-    headers:headers
-  })
-            
-    .then(function(response){
-     // console.log(response);
+
+  axios
+    .delete(`${baseUrl}/posts/${posID}`, {
+      headers: headers,
+    })
+
+    .then(function (response) {
+      // console.log(response);
       hideModal("dexampleModale");
       showAlert("The Post has been deleted seccussfully :)", "success");
-      getPosts()
-
-    }).catch((error) => {
-      
+      getPosts();
+    })
+    .catch((error) => {
       showAlert(`${error.response.data.message}:(`, `danger`);
     });
-
 }
 // =======edit====
-function addbuttonClick(){
-  document.getElementById("postID").value=""
+function addbuttonClick() {
+  document.getElementById("postID").value = "";
   document.getElementById("create_title").innerHTML = "Create New Post";
-  document.getElementById("add").innerHTML="Add This Post"
-  document.getElementById("pimage").src=""
-  document.getElementById("pbody").value=""
-  document.getElementById("ptitle").value=""
+  document.getElementById("add").innerHTML = "Add This Post";
+  document.getElementById("pimage").src = "";
+  document.getElementById("pbody").value = "";
+  document.getElementById("ptitle").value = "";
   let postModal = new bootstrap.Modal(
-   document.getElementById("pexampleModale"),
-   {}
- );
- postModal.toggle();
-
+    document.getElementById("pexampleModale"),
+    {}
+  );
+  postModal.toggle();
 }
 // ========addpostbutton====
 function addPostBtn() {
-  let postid=document.getElementById("postID").value
-  let isCreate=postid==null || postid == ""
-  
-ptitle = document.getElementById("ptitle").value;
-pimage = document.getElementById("pimage").files[0];
-pbody = document.getElementById("pbody").value;
-//console.log(ptitle, pbody);
-const formdata1 = new FormData();
-formdata1.append("title", ptitle);
-formdata1.append("image", pimage);
-formdata1.append("body", pbody);
-let url=``
-const token = localStorage.getItem("token");
-const headers = {
-  "Content-Type": "multipart/form-data",
-  authorization: `Bearer ${token}`,
-};
-if(isCreate){
-      url=`${baseUrl}/posts`
-     
-}else{
-  formdata1.append("_method","put")
-  url=`${baseUrl}/posts/${postid}`
-  
-}
-axios
-  .post(url, formdata1, {
-    headers: headers,
-  })
-  .then((response) => {
-    hideModal("pexampleModale");
-    showAlert("New Post Has been created seccussfully :)", "success");
-    getPosts();
-    //console.log(response);
-  })
-  .catch((error) => {
-    // console.log(error);
-    showAlert(`${error.response.data.message}:(`, `danger`);
-  });
+  let postid = document.getElementById("postID").value;
+  let isCreate = postid == null || postid == "";
 
+  ptitle = document.getElementById("ptitle").value;
+  pimage = document.getElementById("pimage").files[0];
+  pbody = document.getElementById("pbody").value;
+  //console.log(ptitle, pbody);
+  const formdata1 = new FormData();
+  formdata1.append("title", ptitle);
+  formdata1.append("image", pimage);
+  formdata1.append("body", pbody);
+  let url = ``;
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "multipart/form-data",
+    authorization: `Bearer ${token}`,
+  };
+  if (isCreate) {
+    url = `${baseUrl}/posts`;
+  } else {
+    formdata1.append("_method", "put");
+    url = `${baseUrl}/posts/${postid}`;
+  }
+  axios
+    .post(url, formdata1, {
+      headers: headers,
+    })
+    .then((response) => {
+      hideModal("pexampleModale");
+      showAlert("New Post Has been created seccussfully :)", "success");
+      getPosts();
+      //console.log(response);
+    })
+    .catch((error) => {
+      // console.log(error);
+      showAlert(`${error.response.data.message}:(`, `danger`);
+    });
 }
-function profileClicked(){
-  let user=getUser()
+function profileClicked() {
+  let user = getUser();
   // let user=localStorage.getItem("user")
   // alert(user.id)
-  let uSER_id=user.id
+  let uSER_id = user.id;
   // alert(uSER_id)
-  
-  location.href=`profile.html?postID=${uSER_id}`
+
+  location.href = `profile.html?postID=${uSER_id}`;
 }
-function loaderFunc(show=true){
-  if(show){
-    document.getElementById("loader").style.visibility="visible"
-  }else{
-    document.getElementById("loader").style.visibility="hidden"
+function loaderFunc(show = true) {
+  if (show) {
+    document.getElementById("loader").style.visibility = "visible";
+  } else {
+    document.getElementById("loader").style.visibility = "hidden";
   }
 }
 // ===============
-
-
-
 
 //   end HideModal
